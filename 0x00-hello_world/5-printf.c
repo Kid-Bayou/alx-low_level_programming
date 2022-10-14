9dbd3d9122f35394640e3555c17965d50148ab5d
acacia <kidist.t.gebrehiwot@gmail.com>
2022-10-13 17:15:46 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main- will print a statement
+ * Return: 0 if finished correct not otherwise
+ */
+int main(void)
+{
+	printf("\"with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
