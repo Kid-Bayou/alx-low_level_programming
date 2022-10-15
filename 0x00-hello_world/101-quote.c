56b34c78a1c3c345efd48d5795eabc178ac1a0bc
acacia <kidist.t.gebrehiwot@gmail.com>
2022-10-15 19:50:49 +0000

    qoute

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,10 @@
 /**
  * main - prints a phrase
  * followed by a new line to the standard error
- * Return: 1
+ * Return: always 1
  */
 int main(void)
 {
-	write(2,"and that piece of art is useful\" - Dor Korpar, 2015-10-19\n", 59);
-	return(1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
