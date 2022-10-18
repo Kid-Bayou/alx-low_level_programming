90d31ae8eb7742164145a7d9c273cef55e2c2286
acacia <kidist.t.gebrehiwot@gmail.com>
2022-10-17 19:08:14 -0700

    alphabet different

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet in lowercase and uppercase
+ * Return: Always zero
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 'a'; x <= 'z'; x++)
+		putchar(x);
+	for (x = 'A'; x <= 'Z'; x++)
+		putchar(x);
+	putchar('\n');
+	return (0);
+}
