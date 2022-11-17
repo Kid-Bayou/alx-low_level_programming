3ee6e6d6ce1b8536d37978b832f013b55f7627ea
acacia <kidist.t.gebrehiwot@gmail.com>
2022-11-17 03:13:15 +0000

    is lower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,4 +1,19 @@
 #include "main.h"
 
 /**
- * 
+ * _islower - shows 1 if the input is lower case
+ * @c: the character in ASCII code
+ * Return: 1 if lowercase, 0 if not
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
