f057726c06421ac6a68df838b010daeb01165cd4
acacia <kidist.t.gebrehiwot@gmail.com>
2022-11-17 08:52:03 +0000

    print sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of the number
+ * @n: the input number as an integer
+ * Return: 1 if the number is gerater, 0 if its zero, -1 if its less than 1
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+	_putchar('\n');
+}
