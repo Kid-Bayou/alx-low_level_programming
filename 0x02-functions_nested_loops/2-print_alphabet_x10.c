2e527d1af6dee2a4d5713fae5cbecadbbcb040db
acacia <kidist.t.gebrehiwot@gmail.com>
2022-11-17 02:59:32 +0000

    print alphabet x10 _

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,11 +6,12 @@
  */
 void print_alphabet_x10(void)
 {
-	int i, j;
+	int i;
+	char j;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		for (j = 97; j <= 122; j++)
+		for (j = 'a'; j <= 'z'; j++)
 		{
 			_putchar(j);
 		}
