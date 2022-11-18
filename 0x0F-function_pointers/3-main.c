b5c0b975881e75f2899abf4e1dd10ab6c069185b
acacia <kidist.t.gebrehiwot@gmail.com>
2022-11-18 19:07:21 +0000

    op func another

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,33 @@
+#include "3-calc.h"
+
+/**
+ * main - entry point
+ * @argc: argument count
+ * @argv: argumemnts
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int x, y, r;
+	int (*p_func)(int, int);
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	p_func = get_op_func(argv[2]);
+
+	if (p_func == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	x = atoi(argv[1]);
+	y = atoi(argv[3]);
+	r = p_func(x, y);
+
+	printf("%d\n", r);
+	return (0);
+}
