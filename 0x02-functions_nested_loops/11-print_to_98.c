3b3ca1b75b9fafbbd354ed21b7dc4078ddd36b7c
acacia <kidist.t.gebrehiwot@gmail.com>
2022-11-19 15:05:14 +0000

    print to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * print_to_98 - adds two numbers
+ * @n: a value
+ * Return: void
+ */
+void print_to_98(int n)
+{
+	if (n > 98)
+	{
+		while (n >= 98)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+			n--;
+		}
+		printf("\n");
+	}
+	else if (n < 98)
+	{
+		while (n <= 98)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(", ");
+			}
+			n++;
+		}
+		printf("\n");
+	}
+	else
+		printf("%d\n", n);
+}
