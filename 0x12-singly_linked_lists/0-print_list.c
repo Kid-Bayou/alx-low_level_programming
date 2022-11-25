8bb6e7443f19379f61ae25fe9c519d9aa13b54ed
acacia <kidist.t.gebrehiwot@gmail.com>
2022-11-25 20:16:43 +0000

    first

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_list - prints the list
+ * @h: pointer to a sturcutre typedef
+ * Return: the number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	const list_t *temp = h;
+	size_t num_nodes;
+
+	for (num_nodes = 0; temp != NULL; num_nodes++)
+	{
+		if (temp->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", temp->len, temp->str);
+		temp = temp->next;
+	}
+	return (numNodes);
+}
